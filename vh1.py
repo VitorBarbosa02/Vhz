dccf901fc3263d2ecd0c8fa83115fdcb6001842b
VitorBarbosa02 <vitorhugobarbosadorosario@gmail.com>
2025-06-18 10:13:53 -0300

    Add files via upload

---
+++ vh1.py
@@ -0,0 +1,16 @@
+idade =int(input('digite sua idade'))
+
+if idade <=12:
+   print('criança')
+elif idade <17:
+    print('adolescente')
+else:
+    print('adulto')
+
+
+
+
+
+
+
+
---
+++ vh2.py
@@ -0,0 +1,27 @@
+numero=int(input("Digite um numero de 1 a 7:"))
+
+if numero==1:
+   print('domingo')
+elif numero==2:
+    print('segunda')     
+if numero==3:
+  print('terça')
+elif numero==4:
+    print('quarta')
+if numero==5:
+  print('quinta')
+elif numero==6:
+    print('sexta')
+if numero==7:
+  print('sabado')
+
+
+
+
+
+
+
+
+
+
+
